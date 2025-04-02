37122c1c1105a3ce5f4a7cdead8bc961cbcb6f3d
Anshula Gandhi <anshula@users.noreply.github.com>
2025-04-02 14:50:00 +0100

    added sqrt 17 example

--- WebDemo/Pages/DemoPages/Introduction.lean
+++ WebDemo/Pages/DemoPages/Introduction.lean
@@ -1,7 +1,6 @@
 import VersoBlog
 import AutomaticProofGeneralization.AutoGeneralizeTactic
-import Mathlib.Data.Real.Irrational
--- import AutomaticProofGeneralization.Formalizations.irrationality_of_sqrts
+import AutomaticProofGeneralization.Formalizations.irrationality_of_sqrts
 
 open Verso Genre Blog
 
@@ -9,20 +8,35 @@ open Verso Genre Blog
 
 We present an algorithm that takes as its input a theorem, a proof of the theorem, and some aspect of the theorem that can potentially be generalized, and outputs a correct generalization of the theorem and proof.
 
-These demos accompany our paper "Automatically Generalizing Proofs and Statements" submitted to ITP 2025.
+These demos accompany our paper submitted to ITP 2025: "Automatically Generalizing Proofs and Statements" by Anshula Gandhi, Anand Rao Tadipatri, and Timothy Gowers.
 
 # A First Example
 
-Consider the following theorem and proof that the square root of 17 is irrational.
+Consider the following theorem and proof that √17 is irrational.
 
-```leanInit demo
+```leanInit introduction
 ```
 
-For example, given the theorem & proof that √2 is irrational….
-```lean demo
-theorem irrat_sqrt : Irrational (√17) := sorry
-  -- by {apply irrat_def; intros h; obtain ⟨a, b, ⟨copr, h⟩⟩ := h; have a_div : 17 ∣ a := by {have c := (Nat.Prime.dvd_mul (prime_seventeen)).mp ((by apply (Iff.mpr dvd_iff_exists_eq_mul_right); use (b*b); rw [← mul_assoc]; rw [h];): 17 ∣ a*a); cases c; assumption; assumption}; have a_is_pk : ∃ k, a = 17 * k := by {apply (Iff.mp dvd_iff_exists_eq_mul_right) a_div}; obtain ⟨k, hk⟩ := a_is_pk; rw [hk] at h; replace h := Eq.symm h; rw [mul_assoc] at h; rw [mul_assoc] at h; rw [mul_comm 17 k] at h; rw [mul_eq_mul_left_iff] at h; rw [← mul_assoc k k 17] at h; have := Nat.Prime.ne_zero prime_seventeen; cases h with | inl => have b_div : 17 ∣ b := by {have c := (Nat.Prime.dvd_mul (prime_seventeen)).mp ((by apply (Iff.mpr dvd_iff_exists_eq_mul_left); use (k*k))); cases c; assumption; assumption}; have p_dvd_gcd : 17 ∣ Nat.gcd a b := by {apply Iff.mpr Nat.dvd_gcd_iff ⟨a_div, b_div⟩}; clear a_div b_div; rw [copr] at p_dvd_gcd; apply Nat.Prime.not_dvd_one (prime_seventeen) p_dvd_gcd | inr => apply this; assumption}
+```lean introduction
+theorem irrat_sum_sqrt' : Irrational √17 := by
 
+  -- If 17 is irrational, we should not be able to find coprime a and b such that a/b = √17.  That is, a^2 = 17 b^2.
+  apply irrat_def
+
+  -- It follows that 17 | a^2.  Since 17 is prime, 17 | a.
+  rintro ⟨a, b, ⟨copr, h⟩⟩; have a_div : 17 ∣ a := by {have c : 17 ∣ a * a := by {rw [h, mul_assoc]; exact Nat.dvd_mul_right _ _}; rw [Nat.Prime.dvd_mul prime_seventeen] at c; cases c <;> assumption}
+
+  -- So we can write a = 17k for some k.
+  have a_is_pk : ∃ k, a = 17 * k := Iff.mp dvd_iff_exists_eq_mul_right a_div
+
+  -- Plugging  means 17 k^2 = b^2.
+  obtain ⟨k, hk⟩ := a_is_pk; rw [hk] at h; symm at h; rw [mul_assoc, mul_assoc, mul_comm 17 k, mul_eq_mul_left_iff, ← mul_assoc k k 17] at h; simp only [Nat.Prime.ne_zero prime_seventeen, or_false] at h
+
+  -- It follows that 17 | b^2.  Since 17 is prime, 17 | b.
+  have b_div : 17 ∣ b := by {have c : 17 ∣ b * b := by {rw [h]; exact Nat.dvd_mul_left 17 (k * k)}; rw [Nat.Prime.dvd_mul prime_seventeen] at c; cases c <;> assumption}
+
+  -- Now we have a contradiction -- a and b were supposed to be coprime, but 17 divides both.
+  have p_dvd_gcd : 17 ∣ Nat.gcd a b := Iff.mpr Nat.dvd_gcd_iff ⟨a_div, b_div⟩; clear a_div b_div; rw [copr] at p_dvd_gcd; apply Nat.Prime.not_dvd_one prime_seventeen p_dvd_gcd
 ```
 
-If our algorithm is presented with the above theorem and proof, formalized in Lean, and asked to generalize the number 17 by replacing it with an unknown natural number $`n`, it will determine that the only property it used of the number 17 was that it was prime, so its output will be the theorem that if $`n` is prime, then $`\sqrt n`is irrational, together with a correct proof of that theorem.
+If our algorithm is presented with the above theorem and proof, formalized in Lean, and asked to generalize the number 17 by replacing it with an unknown natural number $`n`, it will determine that the only property it used of the number 17 was that it was prime, so its output will be the theorem that if $`n` is prime, then $`\sqrt n` is irrational, together with a correct proof of that theorem.
