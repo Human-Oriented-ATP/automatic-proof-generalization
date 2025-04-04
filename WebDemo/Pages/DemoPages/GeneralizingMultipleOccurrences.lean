c75becd84e6813ae28cd90cc304dfc6a9f3e480a
Anshula Gandhi <anshula@users.noreply.github.com>
2025-04-04 11:26:37 +0100

    stylistic changes & rephrasings

--- WebDemo/Pages/DemoPages/GeneralizingMultipleOccurrences.lean
+++ WebDemo/Pages/DemoPages/GeneralizingMultipleOccurrences.lean
@@ -3,25 +3,32 @@ import AutomaticProofGeneralization.Formalizations.irrationality_of_sqrts
 import Mathlib.Data.Fintype.Pi
 import VersoBlog
 
+set_option pp.showLetValues false
+
 open Verso Genre Blog
 
-#doc (Page) "Generalizing Multiple Occurrences" =>
+#doc (Page) "Generalizing Repeated Constants" =>
 
 ```leanInit generalizingMultipleOccurrences
 ```
 
-In a proof where a constant appears multiple times, the algorithm can generalize each occurrence separately.
+Often, a proof will contain the same constant multiple times.  When we generalize, the proof should tell us whether these instances of the constant must necessarily be equal for the proof to go through, or whether each instance plays a different role in the proof.
 
-Consider the following proof that $`\sqrt{17} + 17` is irrational.
+# Generalizing Instances Separately
+
+
+So, in a proof where a constant appears multiple times, the algorithm can determine when to generalize each occurrence separately.
+
+Consider the following proof that $`17 + \sqrt{17}` is irrational.
 
 ```lean generalizingMultipleOccurrences
-theorem irrat_sum_sqrt : Irrational (17 + Real.sqrt (17:ℕ)) := by
-  -- it suffices to show that `√17` is irrational, since the result of adding a natural number to an irrational number is irrational.
+theorem irrat_sum : Irrational (17 + Real.sqrt (17:ℕ)) := by
 
+  /- It suffices to show that `√17` is irrational,
+     since a natural number plus an irrational is irrational. -/
   apply Irrational.nat_add
 
-  -- the rest of the proof shows that √17 is irrational
-
+  /- The rest of the proof shows that √17 is irrational. -/
   apply irrat_def
   rintro ⟨a, b, ⟨copr, h⟩⟩; have a_div : 17 ∣ a := by {have c : 17 ∣ a * a := by {rw [h, mul_assoc]; exact Nat.dvd_mul_right _ _}; rw [Nat.Prime.dvd_mul prime_seventeen] at c; cases c <;> assumption}
   have a_is_pk : ∃ k, a = 17 * k := Iff.mp dvd_iff_exists_eq_mul_right a_div
@@ -30,45 +37,49 @@ theorem irrat_sum_sqrt : Irrational (17 + Real.sqrt (17:ℕ)) := by
   have p_dvd_gcd : 17 ∣ Nat.gcd a b := Iff.mpr Nat.dvd_gcd_iff ⟨a_div, b_div⟩; clear a_div b_div; rw [copr] at p_dvd_gcd; apply Nat.Prime.not_dvd_one prime_seventeen p_dvd_gcd
 ```
 
-Generalizing this produces a proof that $`\sqrt{p}+n` is irrational for any prime $`p` and natural number $`n`.
+We would not want the generalization to place the primality assumption on both occurences on $`17` in the statement, yielding the overly-specific generalization that $`p+\sqrt{p}` is irrational for any prime $`p`.
+
+Happily, our algorithm yields the stronger generalization that $`n+\sqrt{p}` is irrational for any natural number $`n` and prime $`p`.
 
 ```lean generalizingMultipleOccurrences
-example: ∀ (p n : ℕ), Nat.Prime p → Irrational (n + √p) := by
-  intros p n p_prime
+example: ∀ (p : ℕ), Nat.Prime p → ∀ (n : ℕ), Irrational (n + √p) := by
 
-  /- Find the proof-based generalization, and add it as a theorem in the context. -/
-  autogeneralize (17:ℕ) in irrat_sum_sqrt
+  /- Generalize the `17` in the proof,
+     and add the generalization `irrat_sum.Gen` as a hypothesis. -/
+  autogeneralize (17:ℕ) in irrat_sum
 
-  exact irrat_sum_sqrt.Gen p p_prime n
+  /- Use the generalization to close the goal.-/
+  assumption
 ```
 
-In particular, it does not place the assumption of being prime on both occurences on $`17` in the statement, just on the relevant one.
 
-We can also choose to selectively generalize a specified set of occurrences of a constant.
+We can also choose to selectively generalize a particular occurrence of a constant. Below, we only generalize the occurrence of $`17` under the square root, yielding the generalization that $`17+\sqrt{p}` is irrational for any prime $`p`.
 
 ```lean generalizingMultipleOccurrences
 example: ∀ (p : ℕ), Nat.Prime p → Irrational (17 + √p) := by
-  intros p p_prime
 
-  /- Selectively generalize the occurrence of `17` under the square root. -/
-  autogeneralize (17:ℕ) in irrat_sum_sqrt at occurrences [1]
+  /- Selectively generalize the occurrence of `17` under the square root.
+     and add the generalization `irrat_sum.Gen` as a hypothesis. -/
+  autogeneralize (17:ℕ) in irrat_sum at occurrences [1]
 
-  exact irrat_sum_sqrt.Gen p p_prime
+  /- Use the generalization to close the goal.-/
+  assumption
 ```
+
+# Generalizing Instances Together
 
 If different occurrences of a constant play the same role in the proof, the program automatically detects this and generalizes them as the same constant.
 
 For example, consider the following theorem which proves that the number of functions between two sets of size `3` is `3 ^ 3`.
 
 ```lean generalizingMultipleOccurrences
-/-- The number of functions between two sets of size `3` is `3 ^ 3`. -/
 theorem fun_set {α β} [Fintype α] [Fintype β] [DecidableEq α]
     (α_card : Fintype.card α = 3) (β_card : Fintype.card β = 3) : Fintype.card (α → β) = 3 ^ 3 := by
-  rw [Fintype.card_pi, Finset.prod_const]
-  congr
+
+  rw [Fintype.card_pi, Finset.prod_const]; congr
 ```
 
-Here, the cardinality of $`\alpha` is linked to the base of the exponent, and the cardinality of $`\beta` is linked to the power of the exponent. Generalizing $`3` in this proof creates two variables, one for each pair of linked occurrences.
+Generalizing each of the four instances of $`3` to a different variable here would yield an incorrect statement. Rather, the cardinality of $`\alpha` is linked to the base of the exponent $`3^3`, and the cardinality of $`\beta` is linked to the power of the exponent $`3^3`. Generalizing all four instances of $`3` in this proof creates only two variables, one for each pair of linked occurrences.  The result is the generalization that if |α| = n and |β| = m, then the number of functions $`f : α → β` is $`m^n`.
 
 ```lean generalizingMultipleOccurrences
 example {α β : Type} [Fintype α] [Fintype β] [DecidableEq α] :
@@ -78,8 +89,11 @@ by
 
   let fun_set : Fintype.card α = 3 → Fintype.card β = 3 → Fintype.card (α → β) = 3 ^ 3 := by {intros α_card  β_card; rw [Fintype.card_pi, Finset.prod_const]; congr}
 
-  /- Generalize all occurrences of `3` in the proof. -/
+  /- Generalize all occurrences of `3` in the proof.
+     and add the generalization `fun_set.Gen` as a hypothesis. -/
   autogeneralize 3 in fun_set
 
   exact fun_set.Gen n m
 ```
+
+For details on the technical implementation of this algorithm, please see the paper "Automatically Generalizing Proofs and Statements."
