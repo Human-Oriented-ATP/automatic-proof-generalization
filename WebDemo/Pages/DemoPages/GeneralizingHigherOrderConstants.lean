b6c0b7923f5bf47270399520aa13e17ef2eaa01f
0art0 <18333981+0art0@users.noreply.github.com>
2025-04-13 13:37:10 +0100

    `#check` for Bezout's theorem

--- WebDemo/Pages/DemoPages/GeneralizingHigherOrderConstants.lean
+++ WebDemo/Pages/DemoPages/GeneralizingHigherOrderConstants.lean
@@ -11,6 +11,10 @@ open Classical
 ```leanInit generalizingHigherOrderConstants
 ```
 
+```lean generalizingHigherOrderConstants show:=false
+set_option pp.showLetValues false
+```
+
 Since both Pons's algorithm and our extension assume type-theoretic foundations, generalizing constants like $`\mathbb{R}` or $`*` (multiplication) works just as well as generalizing numerical constants.
 
 # Generalizing a proof of left-cancellation in the integers
@@ -53,98 +57,9 @@ What the algorithm has done here by generalizing the constants $`+`, $`0`, and $
 A more complicated example of this kind is provided by Bézout's theorem from elementary number theory. Our algorithm is capable of generalizing the non-numerical constant $`\mathbb{Z}` in a standard proof of this theorem, which states that the greatest common divisor of two integers $`x` and $`y` can always be expressed as a linear combination of $`x` and $`y` with integer coefficients.
 
 ```lean generalizingHigherOrderConstants
-/-- Bézout's identity states that for any two integers a and b, there exist integers x and y such that their greatest common divisor g can be expressed as a linear combination ax + by = g -/
-theorem bezout_theorem : ∀ (x y : ℤ), y ≠ 0 → ∃ (h k : ℤ), isGCD (h * x + k * y) x y := by
-  intros x y y_neq_0
-
-  -- Consider the set A = {hx + ky | x,y ∈ ℤ}
-  let A := {z : ℤ | ∃ h k : ℤ, z = h * x + k * y}
-
-  have A_add : ∀ a ∈ A, ∀ b ∈ A, a + b ∈ A := by
-    rintro a ⟨h, k, a_eq⟩ b ⟨h', k', b_eq⟩
-    use (discharger := skip) (h + h'), (k + k')
-    rw [a_eq, b_eq]
-    rw [Int.add_assoc, Int.add_left_comm (k * y) _ _, ← Int.add_assoc, ← Int.add_mul, ← Int.add_mul]
-  have A_mul : ∀ a ∈ A, ∀ z : ℤ, z * a ∈ A := by
-    rintro a ⟨h, k, a_eq⟩ z
-    use z * h, z * k
-    rw [a_eq]
-    rw [Int.mul_add, ← Int.mul_assoc, ← Int.mul_assoc]
-
-  -- Consider the set B = {|z| | z ∈ A, |z| ≠ 0} of non-zero absolute values
-  let B := (Int.natAbs '' A) \ {0}
-  -- Show B is non-empty by constructing an element
-  have hB_nonempty : ∃ b : ℕ, b ∈ B := by
-    use (0*x + 1*y).natAbs
-    change (0*x + 1*y).natAbs ∈ Int.natAbs '' A \ {0}
-    rw [Set.mem_diff_singleton]
-    constructor
-    · apply Set.mem_image_of_mem Int.natAbs
-      use (discharger := rfl) 0, 1
-    · rwa [Int.zero_mul, Int.one_mul, Int.zero_add, @Ne.eq_def, Int.natAbs_eq_zero]
-  -- By well-ordering principle on subsets of ℕ, B has a minimal element
-  -- Call that minimal element "d"
-  let Bmin : ℕ := Nat.find hB_nonempty
-  have hBmin_spec : (∃ d ∈ A, d.natAbs = Bmin) ∧ Bmin ≠ 0 := Nat.find_spec hB_nonempty
-  rcases hBmin_spec.1 with ⟨d, hd⟩
-  have hdA := hd.1
-  have hdAbs_eq_Bmin:= hd.2
-  have hBmin_neq_0 := hBmin_spec.2
-  have hdAbs_neq_0 : d.natAbs ≠ 0 := by rwa [← hdAbs_eq_Bmin] at hBmin_neq_0
-  have hd_min : ∀ z ∈ A, z.natAbs = 0 ∨ d.natAbs ≤ z.natAbs := by
-    intro z hz
-    by_cases hzAbs : z.natAbs = 0
-    · left
-      assumption
-    · right
-      have hBmin_min := Nat.find_min' hB_nonempty (m := z.natAbs)
-        ⟨Set.mem_image_of_mem Int.natAbs hz, hzAbs⟩
-      rwa [hdAbs_eq_Bmin]
-
-  have hd_div_A : ∀ a ∈ A, d ∣ a := by
-    intro a ha_A
-    -- By division algorithm, x = qd + r for some q,r with 0 ≤ r < d
-    let q := a / d
-    let r := a % d
-    have a_eq_quotRem : a = q*d + r := Eq.symm (Int.ediv_add_emod' a d)
-    have r_eq : r = (-q)*d + a := by
-      rw [← neg_add_eq_iff_eq_add, Int.neg_mul_eq_neg_mul] at a_eq_quotRem
-      symm; assumption
-    have rAbs_lt_dAbs : r.natAbs < d.natAbs := by
-      apply Int.emod_natAbs_lt_of_nonzero
-      assumption
-    have hr_A : r ∈ A := by
-      rw [r_eq]
-      apply A_add
-      · apply A_mul; assumption
-      · assumption
-    by_cases hr_eq_0 : r.natAbs = (0 : ℕ)
-    · rw [Int.natAbs_eq_zero] at hr_eq_0
-      rw [a_eq_quotRem, hr_eq_0, Int.add_zero]
-      exact Int.dvd_mul_left q d
-    · have hd_min_r := hd_min r hr_A
-      contrapose hd_min_r
-      push_neg
-      constructor <;> assumption
-
-  have hxA : x ∈ A := by use 1, 0; simp only [Int.one_mul, Int.zero_mul, Int.add_zero]
-  have d_dvd_x : d ∣ x := hd_div_A x hxA
-
-  have hyA : y ∈ A := by use 0, 1; simp only [Int.zero_mul, Int.one_mul, Int.zero_add]
-  have d_dvd_y : d ∣ y := hd_div_A y hyA
-
-  obtain ⟨h, k, d_eq⟩ := hdA
-  use (discharger := skip) h, k
-  rw [isGCD]
-  refine' ⟨_, _, _⟩
-  · rwa [← d_eq]
-  · rwa [← d_eq]
-  · rintro c c_dvd_x c_dvd_y
-    rw [Int.dvd_def] at c_dvd_x c_dvd_y ⊢
-    rcases c_dvd_x with ⟨cx, hcx⟩
-    rcases c_dvd_y with ⟨cy, hcy⟩
-    use (h * cx + k * cy)
-    rw [Int.mul_add, Int.mul_left_comm c h _, Int.mul_left_comm c k _, ← hcx, ← hcy]
+/- Bézout's identity states that for any two integers a and b, there exist integers x and y such that
+  their greatest common divisor g can be expressed as a linear combination ax + by = g. -/
+#check (bezout_identity : ∀ a b : ℤ, b ≠ 0 → ∃ x y, isGCD (x * a + y * b) a b)
 ```
 
 Generalizing the integers in this proof produces a definition broadly similar to that of a Euclidean domain from ring theory, which, roughly speaking, means an integral domain with a measure of size that allows one to run the Euclidean algorithm and guarantees that it will terminate.
@@ -152,7 +67,7 @@ Generalizing the integers in this proof produces a definition broadly similar to
 ```lean generalizingHigherOrderConstants
 example := by
   /- Find the proof-based generalization, and add it as a theorem in the context. -/
-  autogeneralize ℤ as R in bezout_theorem
+  autogeneralize ℤ as R in bezout_identity
   assumption
 ```
 
