bb9045391411aa0ce277d5fbd1960350a2dbcba3
Anshula Gandhi <anshula@users.noreply.github.com>
2025-04-15 18:24:38 +0100

    web files

--- WebDemo/Demo.lean
+++
@@ -1,8 +0,0 @@
--- The "library" referenced in "lean_lib Demo"
--- This file is so I can just write "import Demo" and it will import everything.
-
-import WebDemo.Pages.Demo
-import WebDemo.Pages.DemoPages.Introduction
-import WebDemo.Pages.DemoPages.GeneralizingMultipleOccurrences
-import WebDemo.Pages.DemoPages.GeneralizingDependencies
-import WebDemo.Pages.DemoPages.GeneralizingHigherOrderConstants
--- WebDemo/run.sh
+++
@@ -1,7 +0,0 @@
-# lake clean
-# lake build
-# git config --global --unset https.proxy # cheap fix to error saying lake can't connect to github
-cd .. # run from main directory of repo
-rm -rf _out/webdemopage # clear old output
-lake exe webdemo --output _out/webdemopage # add new output
-python3 -m http.server 8800 -d _out/webdemopage # start webserver
