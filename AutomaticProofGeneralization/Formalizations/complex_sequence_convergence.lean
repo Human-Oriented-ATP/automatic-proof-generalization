c3eefaabf1395a27097a89f1b7e51abd11a29f86
0art0 <18333981+0art0@users.noreply.github.com>
2025-05-06 16:31:52 +0100

    Full proof of complex sequence convergence

--- AutomaticProofGeneralization/Formalizations/complex_sequence_convergence.lean
+++ AutomaticProofGeneralization/Formalizations/complex_sequence_convergence.lean
@@ -1,78 +1,55 @@
 import Mathlib
-import LeanAideTools
 
--- #theorem "Let (z_n)_1^infty be a sequence of complex numbers. Suppose that there exists N such that for all n >= N, |z_n| <= |z_{n-1}|/2. Then the sum ∑{n=1}^infty z_n converges."
 open Filter
 
-#check @cauchySeq_finset_iff_nat_tsum_vanishing
-#check @cauchySeq_finset_iff_vanishing_norm
-#check @eventually_atTop
-#check @cauchySeq_finset_of_summable_norm
-
-#check Summable
-#check Finset.range
--- #check Finset.map'
-#check List.range
-
-#check Summable.congr_atTop -- TODO: use this for a WLOG argument
-#check Summable.mul_left
-
-theorem complex_sum_convergence (z : ℕ → ℂ)
+theorem complex_sum_convergence
+  -- `z` is a sequence of complex numbers
+  (z : ℕ → ℂ)
+  -- eventually, the sequence `z` decays by half at each step
   (h : ∀ᶠ n in atTop, ‖z (n+1)‖ ≤ ‖z n‖ / 2)
-    : Summable z := by
-  rw [@summable_iff_cauchySeq_finset]
+  -- To prove: `z` is summable
+  : Summable z := by
+  -- obtain the number `N` after which the sequence starts decaying by half at each step
   rw [@eventually_atTop] at h
-  rcases h with ⟨N, hN⟩
-  have hN : ∀ m : ℕ, ∀ a ≥ N, ‖z (a + m)‖ ≤ ‖z a‖ / 2^m := by
-    intro m a ha
+  obtain ⟨N, hN⟩ := h
+  -- the "tail" of the sequence `z` starting from `N`, i,e., the sequence `z_N, z_{N+1}, z_{N+2}, ...`
+  let z' : ℕ → ℂ := fun n ↦ z (n + N)
+  -- it is sufficient (and equivalent) to show the summability of the tail sequence `z'`
+  rw [← summable_nat_add_iff N]
+  show Summable z'
+  -- rephrasing the condition about the sequence decaying by half at each step in terms of `z'`
+  let h' : ∀ b : ℕ, ‖z' (b + 1)‖ ≤ ‖z' b‖ / 2 := by
+    intro b
+    specialize hN (b + N) (by simp)
+    unfold z'
+    rwa [Nat.add_right_comm]
+  -- to show that a sequence is summable, it suffices to show that it's a Cauchy sequence
+  rw [@summable_iff_cauchySeq_finset]
+  -- the hypothesis `h` implies that the sequence decays exponentially by a factor of `2` after a point
+  have h' : ∀ m : ℕ, ‖z' m‖ ≤ ‖z' 0‖ / 2^m := by
+    intro m
     induction m with
     | zero => simp
     | succ m ih =>
-      calc  ‖z (a + (m + 1))‖
-          = ‖z ((a + m) + 1)‖   := by rw [add_assoc]
-        _ ≤ ‖z (a + m)‖ / 2     := by apply hN; linarith only [ha]
-        _ ≤ (‖z a‖ / 2^m) / 2   := by gcongr
-        _ = ‖z a‖ / 2 ^ (m + 1) := by ring
-  let zBound : ℕ → ℝ :=
-    if hN_zero : N = 0 then
-      (‖z 0‖ / 2 ^ ·)
-    else fun n =>
-      if n < N then
-        -- the maximum of {‖z i‖ | 0 ≤ i < N}
-        let normsUptoN := (List.range N).map (‖z ·‖)
-        normsUptoN.maximum_of_length_pos (by
-          have hnormsUptoN_length : normsUptoN.length = N := by
-            simp only [List.length_map, List.length_range, normsUptoN]
-          rw [hnormsUptoN_length]
-          positivity)
-      else
-        ‖z N‖ / 2 ^ (n - N)
-  apply cauchySeq_finset_of_norm_bounded zBound
+      calc  ‖z' (m + 1)‖
+        _ ≤ ‖z' m‖ / 2           := by apply h'
+        _ ≤ (‖z' 0‖ / 2^m) / 2   := by gcongr
+        _ = ‖z' 0‖ / 2 ^ (m + 1) := by rw [@div_div, @npow_add, @npow_one]
+  -- this function is an upper bound for the norms of the sequence
+  -- it is sufficient to show the summability of this sequence
+  let z'Bound : ℕ → ℝ := fun n ↦ ‖z' 0‖ / 2 ^ n
+  -- the hypothesis `h'` shows that `‖z' i‖ ≤ z'Bound i` for all `i : ℕ`
+  -- it suffices to show that `z'Bound` is summable
+  refine cauchySeq_finset_of_norm_bounded z'Bound ?z'BoundSummable h'
+  -- `z'Bound` is pointwise equal to `fun n => ‖z 0‖ * (1 / 2) ^ n`
+  -- the specific form of the second sequence allows
+  apply Summable.congr (f := fun n => ‖z' 0‖ * (1 / 2) ^ n)
   swap
-  · intro i
-    unfold zBound
-    by_cases hN_zero : N = 0
-    · subst hN_zero
-      specialize hN i 0 (le_refl _)
-      simp_all
-    · simp [hN_zero]
-      by_cases hN_lt : i < N
-      · simp only [hN_lt, ↓reduceIte, List.le_maximum_of_length_pos_iff]
-        refine List.le_maximum_of_mem' ?_
-        refine List.mem_map_of_mem (fun x => ‖z x‖) ?_
-        exact List.mem_range.mpr hN_lt
-      · simp only [hN_lt, ↓reduceIte]
-        specialize hN (i - N) N (le_refl _)
-        rwa [@add_tsub_cancel_iff_le.mpr _] at hN
-        linarith only [hN_lt]
-  · unfold zBound
-    by_cases hN_zero : N = 0
-    · subst hN_zero
-      simp only [↓reduceDIte]
-      apply Summable.congr (f := fun n => ‖z 0‖ * (1 / 2) ^ n)
-      swap
-      · intro x
-        ring_nf
-      apply Summable.mul_left
-      exact summable_geometric_two
-    · sorry
+  · -- prove that `z'Bound` is equal to the other sequence pointwise
+    intro _
+    unfold z'Bound
+    rw [@div_pow, @one_pow, @mul_one_div]
+  · -- the multiplicative constant `‖z' 0‖` can be ignored
+    apply Summable.mul_left
+    -- the sequence `(1 / 2) ^ n` is summable
+    exact summable_geometric_two
