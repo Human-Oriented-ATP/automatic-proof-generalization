1ca96cf9837082843e1111799002471adc78becd
0art0 <18333981+0art0@users.noreply.github.com>
2025-06-27 15:58:13 +0100

    Documentation for `replacePatternWithMVars`

--- AutomaticProofGeneralization/Helpers/ReplacePatternWithMVars.lean
+++ AutomaticProofGeneralization/Helpers/ReplacePatternWithMVars.lean
@@ -15,23 +15,52 @@ local instance {α} : ExceptToEmoji Exception α where
   | .error _ => crossEmoji
   | .ok _ => ""
 
+section ReplacePatternWithMVars
+
+/-!
+
+This section of the file defines `replacePatternWithMVars`,
+a function that traverses an expression replacing
+all instances of a pattern with (different) metavariables and
+abstracting relevant hypotheses.
+
+It is loosely based on the `Lean.Meta.kabstract` from Lean's core library.
+
+The function runs in the `ReplaceM` monad defined below,
+which keeps track of relevant data during the traversal.
+
+-/
+
+/-- The read-only part of the `ReplaceM` monad. -/
 structure ReplaceM.Context where
+  /-- The local context within which new metavariables are defined. -/
   lctx : LocalContext := {}
+  /-- The local instances within which new metavariables are defined. -/
   localInstances : LocalInstances := {}
-  depth : Nat := 0
+  /-- Whether to abstract hypotheses in addition to replacing occurrences of the pattern with metavariables. -/
   abstractHyps : Bool := true
+  /-- Whether to identify constants in the proof that depend on the one being generalized. -/
   generalizeRelatedConstants : Bool := true
+  /-- The current recursion depth of the function. -/
+  depth : Nat := 0
+  /-- The maximum cutoff recursion depth of the function. -/
   cutOffDepth : Nat := 2
 
+/-- The stateful part of the `ReplaceM` monad. -/
 structure ReplaceM.State where
+  /-- A list of terms identified to be conflicting with the one being generalized. -/
   mismatches : List Expr := []
+  /-- Whether the expression has been modified during the traversal. -/
   modified : Bool := false
 
+/-- The `ReplaceM` monad, which keeps track of relevant data needed to abstract instances of a pattern within an expression. -/
 abbrev ReplaceM := ReaderT ReplaceM.Context <| StateT ReplaceM.State MetaM
 
+/-- Adds a list of terms to the list of conflicting terms in the state. -/
 def ReplaceM.addConflictingTerms (terms : List Expr) : ReplaceM Unit := do
   modify fun σ => { σ with mismatches := (terms ++ σ.mismatches).eraseDups }
 
+/-- Return the result if the action changes the `modified` flag to `true`, return `none` otherwise. -/
 def ReplaceM.withReturnIfModified {α} (act : ReplaceM α) : ReplaceM (Option α) := do
   modify ({· with modified := false})
   let result ← act
@@ -40,6 +69,7 @@ def ReplaceM.withReturnIfModified {α} (act : ReplaceM α) : ReplaceM (Option α
   else
     return none
 
+/-- Create a new metavariable of the given type, marking the expression as `modified` in the process. -/
 def ReplaceM.mkExprMVar (type : Expr) (userName : Name := .anonymous) : ReplaceM Expr := do
   let ctx ← read
   let mvar ← mkFreshExprMVarAt ctx.lctx ctx.localInstances type (kind := .synthetic) (userName := userName)
@@ -50,7 +80,7 @@ def ReplaceM.withIncDepth {α} : ReplaceM α → ReplaceM α :=
   withReader fun ctx ↦ { ctx with depth := ctx.depth + 1 }
 
 def ReplaceM.withoutAbstractingHypsOrGeneralizingConstants {α} : ReplaceM α → ReplaceM α :=
-  withReader fun ctx ↦ { ctx with abstractHyps := false, generalizeRelatedConstants := false }
+  withReader ({ · with abstractHyps := false, generalizeRelatedConstants := false })
 
 def ReplaceM.run {α} (m : ReplaceM α) : MetaM (α × ReplaceM.State) := do
   ReaderT.run m { lctx := ← getLCtx, localInstances := ← getLocalInstances } |>.run {}
@@ -60,84 +90,84 @@ def ReplaceM.run' {α} (m : ReplaceM α) : MetaM α := do
 
 open ReplaceM in
 /--
-Replaces all instances of `p` in `e` with a metavariable.
-Roughly implemented like kabstract, with the following differences:
-  kabstract replaces "p" with a bvar, while this replaces "p" with an mvar
-  kabstract replaces "p" with the same bvar, while this replaces each instance with a different mvar
-  kabstract doesn't look for instances of "p" in the types of constants, this does
-  kabstract doesn't look under binders, but this creates LocalDecls so we can still look under binders
+`replacePatternWithMVars` takes in an expression `e` and a pattern `p` as input
+and abstracts all occurrences of `p` in `e` by replacing them with *different* metavariables,
+abstracting relevant hypotheses and identifying related constants to generalize in the process.
 -/
 partial def replacePatternWithMVars (e : Expr) (p : Expr) : ReplaceM Expr := do
-  -- logInfo m!"We are replacing the pattern {p}:{← inferType p} with mvars."
+  -- the type of the pattern being generalized
   let pType ← withReducibleAndInstances <| reduce <| ← inferType p
   trace[Autogeneralize.abstractPattern] m!"Abstracting pattern {p} of type {pType}"
-  -- the "depth" here is not depth of expression, but how many constants / theorems / inference rules we have unfolded
   let rec visit (e : Expr) : ReplaceM Expr := do
     let visitChildren : Unit → ReplaceM Expr := fun _ => do
       match e with
-      -- unify types of metavariables as soon as we get a chance in .app
-      -- that is, ensure that fAbs and aAbs are in sync about their metavariables
       | .app f a         =>
-                            let fAbs ← visit f -- the type
-                            let aAbs ← visit a
-                            if (← read).generalizeRelatedConstants then
-                              let expectedA ← extractArgType fAbs
-                              let inferredA ← inferType aAbs
-                              unless ← liftM <| withoutModifyingState <| isDefEq expectedA inferredA do
-                                trace[TypecheckingErrors] m!"Error in typechecking: aAbs was expected to have type \n\t{expectedA} \nbut has type \n\t{inferredA}"
-                                -- the mismatch is probably caused because something else needs to be generalized
-                                let (_result, problemTerms) ← getTermsToGeneralize expectedA inferredA
-                                trace[TypecheckingErrors] m!"The mismatch can probably be fixed by generalizing the terms {problemTerms}"
-                                ReplaceM.addConflictingTerms (problemTerms.map Prod.snd)
-                            return e.updateApp! fAbs aAbs
+        -- it's possible that modifications to the expression during the traversal create application type mismatches
+        -- so it may be necessary to identify additional terms to generalize to make the overall expression type-correct.
+        let fAbs ← visit f
+        let aAbs ← visit a
+        if (← read).generalizeRelatedConstants then
+          let expectedA ← extractArgType fAbs -- the expected type of the argument
+          let inferredA ← inferType aAbs      -- the actual type of the argument
+          -- if there is an application type mismatch, identify new terms to generalize
+          unless ← liftM <| withoutModifyingState <| isDefEq expectedA inferredA do
+            trace[TypecheckingErrors] m!"Error in typechecking: aAbs was expected to have type \n\t{expectedA} \nbut has type \n\t{inferredA}"
+            -- the mismatch is probably caused because something else needs to be generalized
+            -- candidate terms for generalization are identified through anti-unification
+            let (_result, problemTerms) ← getTermsToGeneralize expectedA inferredA
+            trace[TypecheckingErrors] m!"The mismatch can probably be fixed by generalizing the terms {problemTerms}"
+            -- add the conflicting terms to the state
+            ReplaceM.addConflictingTerms (problemTerms.map Prod.snd)
+        return e.updateApp! fAbs aAbs
       | .mdata _ b       => return e.updateMData! (← visit b)
       | .proj _ _ b      => return e.updateProj! (← visit b)
-      | .letE n t v b _ =>  let tAbs ← visit t
-                            let vAbs ← visit v
-                            -- ensure that the generalized value and type are still in sync
-                            unless ← isDefEq tAbs (← inferType vAbs) do
-                              throwError m!"Expected the type of {vAbs} in `let` statement to be {tAbs}, but got {← inferType vAbs}"
-                            let updatedLet ← withLetDecl n tAbs vAbs fun placeholder => do
-                              let b := b.instantiate1 placeholder
-                              let bAbs ← if (←  liftM <| withoutModifyingState <| isDefEq tAbs t) then
-                                    visit b -- now it's safe to recurse on b (no loose bvars)
-                                  else
-                                    logInfo m!"tAbs {tAbs} and t {t} are not defeq"
-                                    pure b
-                              return ← mkLetFVars (usedLetOnly := false) #[placeholder] bAbs-- put the "n:tAbs" back in the expression itself instead of in an external fvar
+      | .letE n t v b _ =>
+        let tAbs ← visit t
+        let vAbs ← visit v
+        -- ensure that the generalized value and type are still in sync
+        unless ← isDefEq tAbs (← inferType vAbs) do
+          throwError m!"Expected the type of {vAbs} in `let` statement to be {tAbs}, but got {← inferType vAbs}"
+        -- "withLetDecl" temporarily adds "n : tAbs" to context, storing the fvar in placeholder
+        let updatedLet ← withLetDecl n tAbs vAbs fun placeholder => do
+          let b := b.instantiate1 placeholder
+          let bAbs ← if (←  liftM <| withoutModifyingState <| isDefEq tAbs t) then
+                visit b  -- now it's safe to recurse on b (no loose bvars)
+              else
+                logInfo m!"tAbs {tAbs} and t {t} are not defeq"
+                pure b
+          return ← mkLetFVars (usedLetOnly := false) #[placeholder] bAbs-- put the "n:tAbs" back in the expression itself instead of in an external fvar
 
-                            return updatedLet
+        return updatedLet
       | .lam n d b bi     =>
-                              let dAbs ← visit d
-                              --"withLocalDecl" temporarily adds "n : dAbs" to context, storing the fvar in placeholder
-                              let updatedLambda ← withLocalDecl n bi dAbs fun placeholder => do
-                                let b := b.instantiate1 placeholder
-                                let bAbs ←
-                                  if (←  liftM <| withoutModifyingState <| isDefEq dAbs d) then
-                                    visit b-- now it's safe to recurse on b (no loose bvars)
-                                  else
-                                    logInfo m!"dAbs {dAbs} and d {d} are not defeq"
-                                    pure b
-                                return ← mkLambdaFVars #[placeholder] bAbs (binderInfoForMVars := bi) -- put the "n:dAbs" back in the expression itself instead of in an external fvar
+        let dAbs ← visit d
+        -- "withLocalDecl" temporarily adds "n : dAbs" to context, storing the fvar in placeholder
+        let updatedLambda ← withLocalDecl n bi dAbs fun placeholder => do
+          let b := b.instantiate1 placeholder
+          let bAbs ←
+            if (←  liftM <| withoutModifyingState <| isDefEq dAbs d) then
+              visit b  -- now it's safe to recurse on b (no loose bvars)
+            else
+              logInfo m!"dAbs {dAbs} and d {d} are not defeq"
+              pure b
+          return ← mkLambdaFVars #[placeholder] bAbs (binderInfoForMVars := bi) -- put the "n:dAbs" back in the expression itself instead of in an external fvar
 
-                              return updatedLambda
+        return updatedLambda
       | .forallE n d b bi =>
-                              let dAbs ← visit d
-                              --"withLocalDecl" temporarily adds "n : dAbs" to context, storing the fvar in placeholder
-                              let updatedForAll ← withLocalDecl n bi dAbs fun placeholder => do
-                                let b := b.instantiate1 placeholder
-                                let bAbs ← visit b  -- now it's safe to recurse on b (no loose bvars)
-                                return ← mkForallFVars #[placeholder] bAbs (binderInfoForMVars := bi) -- put the "n:dAbs" back in the expression itself instead of in an external fvar
-
-                              return updatedForAll
+        let dAbs ← visit d
+        -- "withLocalDecl" temporarily adds "n : dAbs" to context, storing the fvar in placeholder
+        let updatedForAll ← withLocalDecl n bi dAbs fun placeholder => do
+          let b := b.instantiate1 placeholder
+          let bAbs ← visit b  -- now it's safe to recurse on b (no loose bvars)
+          return ← mkForallFVars #[placeholder] bAbs (binderInfoForMVars := bi) -- put the "n:dAbs" back in the expression itself instead of in an external fvar
+        return updatedForAll
       | e                =>
         return e
 
     withTraceNodeBefore `Autogeneralize.abstractPattern (pure m!"Visiting {e} at depth {(← read).depth}") do
-      -- if the expression "e" is the pattern you want to replace...
       let mctx ← getMCtx
-      if !e.isMVar && (← withReducibleAndInstances <| isDefEq e p) then -- (← liftM <| withoutModifyingState <| isDefEq e p) then
-        -- since the type of `p` may be slightly different each time depending on the context it's in, we infer its type each time
+      -- if the expression "e" matches with the pattern being replaced ...
+      if !e.isMVar && (← withReducibleAndInstances <| isDefEq e p) then
+        -- since the type of `p` may be slightly different each time depending on the context it's in, we specialize it each time
         let pType ← instantiateMVars pType
         setMCtx mctx
         trace[Autogeneralize.abstractPattern] m!"{checkEmoji} Found pattern {p}, replacing with mvar"
@@ -151,15 +181,23 @@ partial def replacePatternWithMVars (e : Expr) (p : Expr) : ReplaceM Expr := do
         if (← read).depth > (← read).cutOffDepth || !(← read).abstractHyps then
           return e'
         else
+          -- if the current expression contains the pattern in its type (but not its term),
+          -- we abstract it as a hypothesis of the generalized type
+
+          -- the type of the expression being generalized
           let e'Type ← inferType e'
           trace[Autogeneralize.abstractPattern] m!"🔍 Inspecting type {e'Type}"
+          -- visiting the type of the expression, replacing any occurrences of the pattern with metavariables
+          -- `withReturnIfModified` will return `none` if the type is not modified
           let some e'TypeAbs ←
             withReturnIfModified <| withIncDepth <| withoutAbstractingHypsOrGeneralizingConstants <|
               visit e'Type | return e'
-            trace[Autogeneralize.abstractPattern] m!"⭐ Generating hypothesis {e'TypeAbs}"
-          mkExprMVar e'TypeAbs
-            -- (userName := mkAbstractedName n)-- mvar for generalized proof
+          trace[Autogeneralize.abstractPattern] m!"⭐ Generating hypothesis {e'TypeAbs}"
+          mkExprMVar e'TypeAbs -- mvar for generalized hypothesis
+            -- (userName := mkAbstractedName n)
   visit e
+
+end ReplacePatternWithMVars
 
 /- Just like kabstract, except abstracts to mvars instead of bvars
   We use this to abstract the theorem TYPE...so we can re-specialize non-abstracted occurrences in the proof.
